12e512d1a73a92250bc1aaeede6f5745d0b17d9a
Kweeka <mtalbi29@yahoo.com>
2022-12-03 14:04:19 +0100

    Fix file path

--- Day 3/part1.js
+++ Day 3/part1.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const file = fs.readFileSync("./input.txt", { encoding: "ascii" })
+import fs from "fs";
+const file = fs.readFileSync(`${process.cwd()}/Day\ 3/input.txt`, { encoding: "ascii" })
 const lines = file.split("\r\n");
 let score = 0
 
@@ -8,14 +8,14 @@ lines.forEach(line => {
 })
 
 
-function getScore(line = "") {
+function getScore(line) {
   const compartments = [line.slice(0, line.length / 2), line.slice(line.length / 2)]
   const chars = compartments[0].split("")
 
   for (let index = 0; index < chars.length; index++) {
     if (compartments[1].includes(chars[index])) {
-      itemAsciiCode = Number(chars[index].charCodeAt(0))
-      if (itemASCIICode > 90) score += Math.abs((121 - itemAsciiCode) - (26 - 1))
+      const itemAsciiCode = Number(chars[index].charCodeAt(0))
+      if (itemAsciiCode > 90) score += Math.abs((121 - itemAsciiCode) - (26 - 1))
       else score += Math.abs((89 - itemAsciiCode) - (52 - 1))
       break;
     }
