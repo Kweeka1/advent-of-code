425c2f449dd1908e6a522f4e84a5bb9736c219b4
Kweeka <mtalbi29@yahoo.com>
2022-12-03 12:23:31 +0100

    Add small adjustment

--- Day 3/part1.js
+++ Day 3/part1.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const file = fs.readFileSync("./input.txt", { encoding: "ascii" })
-const l = file.split("\r\n");
+const lines = file.split("\r\n");
 let score = 0
 
-l.forEach(line => {
+lines.forEach(line => {
   getScore(line)
 })
 
@@ -14,12 +14,9 @@ function getScore(line = "") {
 
   for (let index = 0; index < chars.length; index++) {
     if (compartments[1].includes(chars[index])) {
-      itemScore = Number(chars[index].charCodeAt(0))
-      if (itemScore > 90) {
-        score += Math.abs((121 - itemScore) - (26 - 1))
-      } else {
-        score += Math.abs((89 - itemScore) - (52 - 1))
-      }
+      itemAsciiCode = Number(chars[index].charCodeAt(0))
+      if (itemASCIICode > 90) score += Math.abs((121 - itemAsciiCode) - (26 - 1))
+      else score += Math.abs((89 - itemAsciiCode) - (52 - 1))
       break;
     }
   }
