176379e3d458ed2066fa6237193eca6dc441372c
Kweeka <mtalbi29@yahoo.com>
2022-12-04 10:29:58 +0100

    Update part1.js

--- Day 4/part1.js
+++ Day 4/part1.js
@@ -5,9 +5,9 @@ let score = 0;
 
 lines.forEach(line => {
   const match = line.match(/\d+/g);
-  const rows = match.map(num => Number(num)).sort((a, b) => a - b); 
+  const row = match.map(num => Number(num)).sort((a, b) => a - b); 
   for (let index = 0; index < match.length; index += 2) {
-    if (Number(match[index]) === rows.at(0) && Number(match[index + 1]) === rows.at(-1)) {
+    if (Number(match[index]) === row.at(0) && Number(match[index + 1]) === row.at(-1)) {
       score++
       break;
     }
