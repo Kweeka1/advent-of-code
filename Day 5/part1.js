6097454f9b8a74c57240806231b99c6f15ba04b9
Kweeka <mtalbi29@yahoo.com>
2022-12-10 11:42:33 +0100

    Update part1.js

--- Day 5/part1.js
+++ Day 5/part1.js
@@ -3,7 +3,7 @@ const file = fs.readFileSync(`${process.cwd()}/Day\ 5/input.txt`, { encoding: "a
 const lines = file.split("\r\n");
 const splited = lines.findIndex(line => line === '');
 
-const stack = {}
+const stacks = {}
 const supplies = lines
   .slice(0, splited - 1)
   .map(line => line.replace(/\s{4}/g, "[*]").replace(/\s/g, ''))
@@ -14,10 +14,10 @@ for (let i = 0; i < supplies.length; i++) {
   for (let j = 0; j < supplies[i].length; j = j + 3) {
     const crate = supplies[i].slice(j, j + 3)
     if (crate !== "[*]") {
-      if (stack[(j / 3) + 1]) {
-        stack[(j / 3) + 1].push(crate)
+      if (stacks[(j / 3) + 1]) {
+        stacks[(j / 3) + 1].push(crate)
       } else {
-        stack[(j / 3) + 1] = [crate]
+        stacks[(j / 3) + 1] = [crate]
       }
     }
   }
@@ -25,12 +25,12 @@ for (let i = 0; i < supplies.length; i++) {
 
 moves.forEach(move => {
   for (let index = 0; index < move[0]; index++) {
-    const crate = stack[move[1]].shift();
-    stack[move[2]].unshift(crate);
+    const crate = stacks[move[1]].shift();
+    stacks[move[2]].unshift(crate);
   }
 })
 
-const result = Object.values(stack).reduce((acc, next) => {
+const result = Object.values(stacks).reduce((acc, next) => {
   return acc + next[0].replace(/(\[)|(\])/g, '')
 }, '')
 
